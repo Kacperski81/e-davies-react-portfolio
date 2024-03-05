48a27f601e5f90b12cd2cb34e07ecd887c81f4ba
Marcin Kacperski <kacperski81@gmail.com>
2024-03-05 08:50:24 +0000

    abc

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/e-davies-react-portfolio/',
   build: {
-    outDir: "./"
-  }
+    outDir: "dist"
+  },
+  plugins: [react()]
 })
+
